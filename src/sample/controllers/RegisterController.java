3960c4bf33c14c218419ddfbc1d52b71ce8bda5d
andreipetrut99 <andreipetrut99@users.noreply.github.com>
2020-12-28 12:50:41 +0200

    fixed register's bugs

--- src/sample/controllers/RegisterController.java
+++ src/sample/controllers/RegisterController.java
@@ -6,17 +6,20 @@ import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
 import database.Database;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.awt.event.KeyEvent;
+import java.net.URL;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.ResourceBundle;
 
-public class RegisterController {
+public class RegisterController implements Initializable {
     @FXML
     JFXTextField usernameTextField;
     @FXML
@@ -39,6 +42,16 @@ public class RegisterController {
     ImageView checkRegex;
     @FXML
     ImageView checkMatch;
+    @FXML
+    ImageView checkUser;
+    @FXML
+    Label takenText;
+    @FXML
+    Label wrongUsername;
+    @FXML
+    Label wrongPassword;
+    @FXML
+    Label wrongMatch;
 
     @FXML
     private void registerUser() {
@@ -73,7 +86,7 @@ public class RegisterController {
                 "'), '" + id + "')";
 
         try {
-            if (checkRegex.getOpacity() == 1 && checkMatch.getOpacity() == 1) {
+            if (checkConditions()) {
                 Database.getInstance().runUpdateSql(query);
                 Stage stage = (Stage) registerButton.getScene().getWindow();
                 stage.close();
@@ -103,5 +116,69 @@ public class RegisterController {
         } else {
             checkMatch.setStyle("-fx-opacity: 0");
         }
+    }
+
+    @FXML
+    public void checkNick() throws SQLException {
+        String query = "SELECT U_Id FROM users WHERE UserId = '"
+                + usernameTextField.getText() + "'";
+        ResultSet rs = null;
+        try {
+            rs = Database.getInstance().runSql(query);
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        if (!rs.next() && usernameTextField.getText()!="") {
+            checkUser.setStyle("-fx-opacity: 1");
+            takenText.setStyle("-fx-opacity: 0");
+        } else {
+            checkUser.setStyle("-fx-opacity: 0");
+            takenText.setStyle("-fx-opacity: 1");
+        }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        Database.getInstance();
+    }
+
+    public boolean checkConditions() {
+        if (checkUser.getOpacity() == 1 && checkMatch.getOpacity() == 1
+                && checkRegex.getOpacity() == 1) {
+            return true;
+        }
+        if (checkUser.getOpacity() == 0) {
+            new Thread(() -> {
+                wrongUsername.setStyle("-fx-opacity: 1;");
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException interruptedException) {
+                    interruptedException.printStackTrace();
+                }
+                wrongUsername.setStyle("-fx-opacity: 0");
+            }).start();
+        } else if (checkRegex.getOpacity() == 0) {
+            new Thread(() -> {
+                wrongPassword.setStyle("-fx-opacity: 1;");
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException interruptedException) {
+                    interruptedException.printStackTrace();
+                }
+                wrongPassword.setStyle("-fx-opacity: 0");
+            }).start();
+        } else {
+            new Thread(() -> {
+                wrongMatch.setStyle("-fx-opacity: 1;");
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException interruptedException) {
+                    interruptedException.printStackTrace();
+                }
+                wrongMatch.setStyle("-fx-opacity: 0");
+            }).start();
+        }
+        return false;
     }
 }
